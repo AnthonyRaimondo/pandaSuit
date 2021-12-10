63ee5b58d92679e8b027a4ae825a41c90ca8c116
anthonyraimondo <anthonyraimondo7@gmail.com>
2021-12-10 09:33:28 -0600

    updated documentation

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='pandaSuit',
-    version='0.0.7',
+    version='0.0.8',
     packages=find_packages(where='src/main/python', exclude='test'),
     package_dir={'': 'src/main/python'},
     url='https://github.com/AnthonyRaimondo/pandaSuit',
     license='MIT',
     author='Anthony Raimondo',
     author_email='anthonyraimondo7@gmail.com',
-    description='pandas wrapper and extension for DataFrame manipulation, statistics, and plotting',
+    description='pandas extension for DataFrame manipulation, statistics, and plotting',
     install_requires=["pandas", "scikit-learn"],
     long_description=README,
     long_description_content_type="text/markdown",
