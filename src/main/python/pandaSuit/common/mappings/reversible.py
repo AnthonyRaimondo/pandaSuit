09e208e06a2e368f3998160767202ecd4a1b7700
anthonyraimondo <anthonyraimondo7@gmail.com>
2021-12-26 11:20:56 -0500

    reversible .insert() method

--- src/main/python/pandaSuit/common/mappings/reversible.py
+++ src/main/python/pandaSuit/common/mappings/reversible.py
@@ -1,7 +1,10 @@
-from pandaSuit.common.constant.decorators import UPDATE, APPEND
+import pandas
+
+from pandaSuit.common.constant.decorators import UPDATE, APPEND, INSERT
+from pandaSuit.common.util.list_operations import create_index_list
 
 
-def intermediate_update_args(kwargs):
+def intermediate_update_args(kwargs: dict) -> dict:
     return {
         "column": kwargs.get("column"),
         "row": kwargs.get("row"),
@@ -9,7 +12,7 @@ def intermediate_update_args(kwargs):
     }
 
 
-def update_args(kwargs):
+def update_args(kwargs: dict) -> dict:
     return {
         "column": kwargs.get("column"),
         "row": kwargs.get("row"),
@@ -17,7 +20,7 @@ def update_args(kwargs):
     }
 
 
-def append_args(kwargs):
+def append_args(kwargs: dict) -> dict:
     if "row" in kwargs:
         return {
             "row": -1
@@ -28,14 +31,31 @@ def append_args(kwargs):
         }
 
 
+def insert_args(kwargs: dict) -> dict:
+    index = kwargs.get("index")
+    if kwargs.get("row") is not None:
+        if isinstance(kwargs.get("row"), pandas.DataFrame):
+            args = {"row": create_index_list(start=index, stop=index+kwargs.get("row").shape[0])}
+        else:
+            args = {"row": index}
+    else:
+        if isinstance(kwargs.get("column"), pandas.DataFrame):
+            args = {"column": create_index_list(start=index, stop=index+kwargs.get("column").shape[1])}
+        else:
+            args = {"column": index}
+    return args
+
+
 REVERSE_MAPPING = {
     UPDATE: "update",
-    APPEND: "remove"
+    APPEND: "remove",
+    INSERT: "remove"
 }
 
 REVERSE_ARGS = {
     UPDATE: update_args,
-    APPEND: append_args
+    APPEND: append_args,
+    INSERT: insert_args
 }
 
 INTERMEDIATE_REVERSE_MAPPING = {
