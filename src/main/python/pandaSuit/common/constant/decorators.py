710e826e43c2eae939ee886ab5e77a7e79eee557
anthonyraimondo <anthonyraimondo7@gmail.com>
2022-01-02 13:47:39 -0500

    added in place operators and manipulation log

--- src/main/python/pandaSuit/common/constant/decorators.py
+++ src/main/python/pandaSuit/common/constant/decorators.py
@@ -6,6 +6,10 @@ APPEND = "append"
 INSERT = "insert"
 REMOVE = "remove"
 RESET = "reset"
+IADD = "__iadd__"
+ISUB = "__isub__"
+IMUL = "__imul__"
+IDIV = "__itruediv__"
 
 # other methods
 SELECT = "select"
